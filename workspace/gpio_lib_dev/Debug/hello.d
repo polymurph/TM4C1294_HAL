31a23723e0287e1e9cfcfe2ea2fd7b6049c7a5c3
moflash <eddyed.k@gmail.com>
2019-10-06 16:57:40 +0200

    added basic structure of project

---
+++ workspace/gpio_lib_dev/Debug/hello.d
@@ -0,0 +1,21 @@
+# FIXED
+
+hello.obj: ../hello.c
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdio.h
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_ti_config.h
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/linkage.h
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+hello.obj: C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+hello.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/tm4c1294ncpdt.h
+
+../hello.c: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdio.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_ti_config.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/linkage.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+C:/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/tm4c1294ncpdt.h: 
---
+++ workspace/gpio_lib_dev/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <inc/tm4c1294ncpdt.h>
+
+/**
+ * hello.c
+ */
+int main(void)
+{
+	
+	return 0;
+}
